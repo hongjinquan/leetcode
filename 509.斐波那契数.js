fff5222f37cf9bbf7db0c7c881acd09148c78334
hongjinquan <hongjinquan@kaochong.com>
2020-12-16 15:53:27 +0800

    chore: new way

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -16,13 +16,39 @@ var fib = function (n) {
      * 1、时间复杂度：o(n^2) * o(1) = o(n^2)
      * 2、空间复杂度：o(1)
      */
+    /*  if (n == 0) {
+         return 0
+     }
+     if (n == 1 || n == 2) {
+         return 1
+     }
+     return fib(n - 1) + fib(n - 2) */
+
+    /**
+     * 方法二：备忘录方式，解决重复计算问题
+     * 复杂度：
+     * 1、时间复杂度：o(n) * o(1) = o(n)
+     * 2、空间复杂度：o(n)
+     */
+    const res = noted([], n)
+    return res;
+    /**
+     * 方法三：DP table 方式
+     */
+};
+
+const noted = (temp, n) => {
     if (n == 0) {
-        return 0
+        return 0;
     }
     if (n == 1 || n == 2) {
-        return 1
+        return 1;
     }
-    return fib(n - 1) + fib(n - 2)
-};
+    if (temp[n]) {
+        return temp[n]
+    }
+    temp[n] = noted(temp, n - 1) + noted(temp, n - 2)
+    return temp[n]
+}
 // @lc code=end
 
