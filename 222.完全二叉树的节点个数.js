188a50bffd4dc98704e31b69556d016043cf4562
hongjinquan <hongjinquan@kaochong.com>
2021-01-04 22:30:40 +0800

    feat: 完全二叉树的节点个数 新方法

--- 222.完全二叉树的节点个数.js
+++ 222.完全二叉树的节点个数.js
@@ -40,8 +40,44 @@ var countNodes = function (root) {
     }
     return counter; */
     /**
-     * 
+     * 二、二分查找 + 位运算
      */
+    if (root == null) {
+        return 0
+    }
+    // 先获取树的深度
+    let level = 0;
+    let node = root
+    while (node.left != null) {
+        level += 1;
+        node = node.left
+    }
+    // 计算最下一层节点的最大数，使用二分法进行查找
+    let low = 1 << level, high = (1 << (level + 1)) - 1;
+    while (low < high) {
+        const mid = Math.floor((high - low + 1) / 2) + low
+        if (nodeExists(root, level, mid)) {
+            low = mid;
+        } else {
+            high = mid - 1
+        }
+    }
+    return low;
+
 };
+
+const nodeExists = (root, level, k) => {
+    let bits = 1 << (level - 1);
+    let node = root;
+    while (node !== null && bits > 0) {
+        if (!(bits & k)) {
+            node = node.left;
+        } else {
+            node = node.right;
+        }
+        bits >>= 1;
+    }
+    return node !== null;
+}
 // @lc code=end
 
